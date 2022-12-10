e7e0988d070fea830b16bd4268736edc16bffbec
SaiKiran <saikiran@ibhubs.co>
2022-12-10 22:09:11 +0530

    Added UI elements in overlay UI and took reference of them in a singleton class

---
+++ Assets/_Scripts/MonoBehaviours/EnergySystemAndRobotSpawnCtrl.cs
@@ -0,0 +1,57 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum SpawnCategory
+{
+    ROBOT_CATEGORY_1,
+    ROBOT_CATEGORY_2,
+    ROBOT_CATEGORY_3,
+    ENERGY_SYSTEM
+}
+
+public class EnergySystemAndRobotSpawnCtrl : MonoBehaviour
+{
+    public static EnergySystemAndRobotSpawnCtrl instance;
+
+    public SpawnCategory spawnCategory;
+
+    public Button randomSpawnButton;
+    public TMP_Dropdown spawnCategoryDropDown;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        populateSpawnCategoryList();
+        randomSpawnButton.onClick.AddListener(onClickOfRandomSpawnButton);
+    }
+
+    private void populateSpawnCategoryList()
+    {
+        spawnCategoryDropDown.options.Clear();
+        spawnCategoryDropDown.options.Add(new TMP_Dropdown.OptionData() { text = SpawnCategory.ROBOT_CATEGORY_1.ToString() });
+        spawnCategoryDropDown.options.Add(new TMP_Dropdown.OptionData() { text = SpawnCategory.ROBOT_CATEGORY_2.ToString() });
+        spawnCategoryDropDown.options.Add(new TMP_Dropdown.OptionData() { text = SpawnCategory.ROBOT_CATEGORY_3.ToString() });
+        spawnCategoryDropDown.options.Add(new TMP_Dropdown.OptionData() { text = SpawnCategory.ENERGY_SYSTEM.ToString() });
+    }
+
+    public SpawnCategory getSelectedSpawnCategory()
+    {
+        SpawnCategory spawnCategory;
+        Enum.TryParse<SpawnCategory>(spawnCategoryDropDown.options[spawnCategoryDropDown.value].text, out spawnCategory);
+        return spawnCategory;
+    }
+
+    private void onClickOfRandomSpawnButton()
+    {
+
+    }
+}
